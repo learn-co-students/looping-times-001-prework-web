990a30e323f345bec42e65d89584f4b84f1f09be
ashante <ashantejr@gmail.com>
2015-11-18 22:23:06 +0000

    11-18-15

--- times.rb
+++ times.rb
@@ -1,24 +1,7 @@
 def using_times
-  5.times do
-    puts "This isn't as fun as you think it is."
-  end
-end
-
-puts using_times
-
-def hiding_jewels
-  3.times do 
-    puts "Hiding 10 stolen jewels."
-    jewels_in_bag = jewels_in_bag - 10
-  end
-end
-
-puts hiding_jewels
-
-def wingardium
   7.times do
     puts "Wingardium Leviosa"
   end
 end
 
-puts wingardium
+puts using_times

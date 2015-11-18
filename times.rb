a6329f8bb68d2a81d31d65a4b01ffd89d433c347
ashante <ashantejr@gmail.com>
2015-11-18 22:19:06 +0000

    This wasn't that bad.

--- times.rb
+++ times.rb
@@ -1,7 +1,24 @@
 def using_times
-	#your code here
+  5.times do
+    puts "This isn't as fun as you think it is."
+  end
 end
 
+puts using_times
 
+def hiding_jewels
+  3.times do 
+    puts "Hiding 10 stolen jewels."
+    jewels_in_bag = jewels_in_bag - 10
+  end
+end
 
+puts hiding_jewels
 
+def wingardium
+  7.times do
+    puts "Wingardium Leviosa"
+  end
+end
+
+puts wingardium
